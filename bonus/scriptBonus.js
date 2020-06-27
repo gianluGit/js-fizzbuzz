e5cabc96fd8aec92aed6464d1abfdbaa1bac81c7
Gianluca <gianlucarecchia1993@gmail.com>
2020-06-27 11:01:51 +0200

    fizzbuzz- aggiunta classe hidden al button

--- bonus/scriptBonus.js
+++ bonus/scriptBonus.js
@@ -4,6 +4,7 @@ btn.addEventListener("click", function(){
   var numeri = document.getElementById('title');
 
 
+
   for (var i = 1; i <= 100; i++) {
     if ((i%3 == 0) && (i%5 == 0)) {
       numeri.innerHTML += "<li class=\"fizzbuzz\">FizzBuzz</li>";
@@ -18,4 +19,5 @@ btn.addEventListener("click", function(){
       numeri.innerHTML += "<li>" + i + "</li>";
     }
   }
+  btn.className = "hidden";
 });
